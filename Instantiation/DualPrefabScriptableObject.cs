0227b722a950dcb9874dd91469ddf9222e3c9ab9
Vítor Fernandes <vitor_resende@ymail.com>
2019-02-05 00:34:28 -0200

    Create DualPrefabScriptableObject.cs

---
+++ Instantiation/DualPrefabScriptableObject.cs
@@ -0,0 +1,19 @@
+﻿using G3P2.Transformation;
+using UnityEngine;
+
+namespace G3P2.Instantiation
+{
+    [CreateAssetMenu(fileName = "new Dual Prefab", menuName = "G3P2/Dual Prefab")]
+    public sealed class DualPrefabScriptableObject : ScriptableObject
+    {
+        [SerializeField]
+        private TransformSource _source;
+        [SerializeField]
+        private TransformTarget _target;
+
+        public void Instantiate(Transform sourceParent, Transform targetParent)
+        {
+            Instantiate(_target, targetParent).SubscribeTo(Instantiate(_source, sourceParent));
+        }
+    }
+}
